cd3846daeadcaf7f8b7dbd922137bbd416198762
Ryan Dunwoody <me@ryandunwoody.com>
2021-01-12 12:35:36 +0800

    Update container setup script to get libs from github

--- build/build-from-noetic/scripts/container-setup.sh
+++ build/build-from-noetic/scripts/container-setup.sh
@@ -22,23 +22,34 @@ apt-get install -y build-essential
 apt-get install -y python3-pip python3-opencv python3-yaml
 apt-get install -y libv4l-dev
 
-# random tools for development purposes
-apt-get install -y nano git
-#apt-get install -y wget i2c-tools
+# various tools for development purposes
+apt-get install -y nano git wget i2c-tools
+
+# get pi-top SDK libs
+cd /home/
+git clone https://github.com/pi-top/pi-top-Python-Common-Library
+git clone https://github.com/pi-top/pi-top-Python-SDK
+
+# install common library
+cd pi-top-Python-Common-Library
+pip3 install .
+python3 setup.py
+
+# install SDK
+cd /home/pi-top-Python-SDK/
+pip3 install .
+python3 setup.py
 
 # install ros packages
-apt-get install -y ros-noetic-tf
-apt-get install -y ros-noetic-tf2-tools
-apt-get install -y ros-noetic-usb-cam
-apt-get install -y ros-noetic-image-view
-apt-get install -y ros-noetic-image-transport
+apt-get install -y ros-noetic-tf ros-noetic-tf2-tools
+apt-get install -y ros-noetic-usb-cam ros-noetic-image-view ros-noetic-image-transport
 apt-get install -y ros-noetic-compressed-image-transport
 apt-get install -y ros-noetic-cv-bridge # this is a biggy
 #apt-get install -y ros-noetic-cv-camera  # build this one from our own source
 apt-get install -y ros-noetic-opencv-apps  # face tracking, people tracking and more
 apt-get install -y python3-rosdep
 
-# install hostname resolver (this doesn't work to fix for now but might with some config)
+# install hostname resolver
 apt-get install -y avahi-daemon
 
 # install tools for server
@@ -48,8 +59,7 @@ sed -i 's/root \/var\/www\/html/root \/home\/catkin_ws\/src\/robotics_web_contro
 service nginx restart
 
 # install python packages
-pip3 install cython
-pip3 install gpiozero smbus2 numpy
+pip3 install cython gpiozero smbus2 numpy
 pip3 install imageio
 pip3 install PyV4L2Camera
 pip3 install simple_pid
@@ -58,12 +68,20 @@ pip3 install simple_pid
 source /ros_entrypoint.sh
 cd /home/catkin_ws/src || exit
 catkin_init_workspace
+
 # chmod python files so they can execute
-#chmod +x /home/catkin_ws/src/ros-rover/chmod_python_files.sh
+chmod +x /home/catkin_ws/src/robotics_apps/src/controller.py
+chmod +x /home/catkin_ws/src/robotics_apps/src/line_follower_node.py
+chmod +x /home/catkin_ws/src/robotics_apps/src/math_functions.py
+chmod +x /home/catkin_ws/src/robotics_apps/src/vision_functions.py
+chmod +x /home/catkin_ws/src/robotics_apps/src/vision_functions.py
+chmod +x /home/catkin_ws/src/robotics_base/chassis_imu/src/imu_publisher.py
+chmod +x /home/catkin_ws/src/robotics_base/chassis_move/src/chassis_move.py
+chmod +x /home/catkin_ws/src/robotics_base/chassis_odom/src/odom_publisher.py
+chmod +x /home/catkin_ws/src/robotics_base/chassis_servos/src/chassis_servos.py
 #bash /home/catkin_ws/src/ros-rover/chmod_python_files.sh
 
-# need ros-noetic-web-video-server  and async_web_server_cpp but cant find it on noetic
-# could put this in its own directory or into main ROS directory in future
+# need ros-noetic-web-video-server and async_web_server_cpp but cant find it on noetic
 cd /home/catkin_ws/src || exit
 git clone https://github.com/GT-RAIL/async_web_server_cpp
 git clone https://github.com/RobotWebTools/web_video_server
@@ -73,9 +91,10 @@ source /home/catkin_ws/devel/setup.bash
 
 echo 'source /home/catkin_ws/devel/setup.bash' >> ~/.bashrc
 
-# install ssh server and configure so pycharm can deploy
+# install ssh server for code deployment
 apt-get install -y openssh-server
 mkdir /var/run/sshd
+# change root password to pi-top
 echo 'root:pi-top' | chpasswd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 echo "export VISIBLE=now" >> /etc/profile
