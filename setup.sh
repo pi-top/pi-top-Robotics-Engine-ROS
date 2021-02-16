8b698bc96cff4486b8a84b319556e09f4f52cd80
Ryan Dunwoody <me@ryandunwoody.com>
2021-02-16 21:36:08 +0800

    Update setup to remove commented out lines

--- setup.sh
+++ setup.sh
@@ -5,26 +5,25 @@ set -e
 set -o xtrace
 
 # image name:tag
-IMAGE_NAME=pt-ros:0.0.0
+IMAGE_NAME=pt-ros-dev:0.0.1
 
-#sudo apt update
-#if [ -x "$(command -v docker)" ]; then
-#    echo "Already installed"
-#    # add command to update if necessary
-#else
-#    echo "Install docker"
-#    # command
-#    curl -fsSL https://get.docker.com -o get-docker.sh
-#    sudo sh get-docker.sh
-#    sudo usermod -aG docker pi
-#fi
-
+sudo apt update
+if [ -x "$(command -v docker)" ]; then
+    echo "Already installed"
+    # add command to update if necessary
+else
+    echo "Install docker"
+    # command
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker pi
+fi
 
 # Need to fix docker GPG key issue for Raspberry Pi
 # This is not yet available in Debian's stable repos so download from here instead
-#curl -fsSL http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.4.4-1~bpo10+1_armhf.deb \
-#      -o libseccomp2_2.4.4-1~bpo10+1_armhf.deb
-#sudo dpkg -i libseccomp2_2.4.4-1~bpo10+1_armhf.deb
+curl -fsSL http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.4.4-1~bpo10+1_armhf.deb \
+      -o libseccomp2_2.4.4-1~bpo10+1_armhf.deb
+sudo dpkg -i libseccomp2_2.4.4-1~bpo10+1_armhf.deb
 
 # build docker image
 sudo docker build -t $IMAGE_NAME -f Dockerfile .
